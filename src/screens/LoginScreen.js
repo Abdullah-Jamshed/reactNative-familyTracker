74fbbadda5b10097205633098fcf545d9e531c8a
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-02-18 23:39:31 +0500

    updates

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -3,6 +3,7 @@ import {
   View,
   Text,
   TextInput,
+  TouchableOpacity,
   SafeAreaView,
   Dimensions,
   StyleSheet,
@@ -33,6 +34,9 @@ const LoginScreen = () => {
           textContentType="password"
           secureTextEntry={true}
         />
+        <TouchableOpacity style={styles.button} activeOpacity={0.8}>
+          <Text style={styles.buttonText}>Sign In</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
@@ -60,6 +64,19 @@ const styles = StyleSheet.create({
     paddingVertical: 5,
     paddingHorizontal: 5,
     marginVertical: 10,
+  },
+  button: {
+    backgroundColor: 'red',
+    width: width / 1.3,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    color: '#ffffff',
+    fontSize: 15,
   },
 });
 
