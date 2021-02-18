e614bdf411cb11f8bca56f735561cf7f1eab744d
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-02-18 23:34:49 +0500

    updates

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,15 +1,38 @@
 import React from 'react';
-import {View, Text, TextInput, SafeAreaView, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  SafeAreaView,
+  Dimensions,
+  StyleSheet,
+} from 'react-native';
 import {connect} from 'react-redux';
 
 // Screen
 //Component
 // Redux
+
+const {width, height} = Dimensions.get('window');
+
 const LoginScreen = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={styles.container}>
-        <Text>Login Screen</Text>
+        <View style={styles.heading}>
+          <Text style={styles.headingText}>Family Tracker</Text>
+        </View>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Email"
+          textContentType="emailAddress"
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Password"
+          textContentType="password"
+          secureTextEntry={true}
+        />
       </View>
     </SafeAreaView>
   );
@@ -21,6 +44,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#ffffff',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  heading: {
+    position: 'absolute',
+    top: 60,
+  },
+  headingText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  textInput: {
+    borderBottomWidth: 1.5,
+    borderBottomColor: 'red',
+    width: width / 1.3,
+    paddingVertical: 5,
+    paddingHorizontal: 5,
+    marginVertical: 10,
   },
 });
 
