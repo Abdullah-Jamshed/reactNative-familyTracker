80367d6a364a5f4b4a973ef3909224a483dec075
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-02-18 22:52:04 +0500

    upsates

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View,Text, TextInput, StyleSheet} from 'react-native';
+
+const LoginScreen = () => {
+  return <View></View>;
+};
+
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:"#fffff"
+    }
+})
+
+export default LoginScreen;
