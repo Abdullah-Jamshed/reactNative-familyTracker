ca76796ba3bfbc2405c43d16ce7112f7a595ef70
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-02-21 02:14:41 +0500

    updates

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,23 +1,82 @@
 import React from 'react';
-import {View} from 'react-native';
+import {Group, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
 
 import TabBar from './TabBar';
 import HomeScreen from '../screens/HomeScreen';
 import MapScreen from '../screens/MapScreen';
 import GroupScreen from '../screens/GroupScreen';
+import GroupDetailScreen from '../screens/GroupDetailScreen';
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+// const GroupStack = () => {
+//   return (
+//     <Stack.Navigator headerMode={false}>
+//       <Stack.Screen name="GroupStack" component={GroupScreen} />
+//       <Stack.Screen name="GroupDetailStack" component={GroupDetailScreen} />
+//     </Stack.Navigator>
+//   );
+// };
+
+// const Navigation = () => {
+//   return (
+//     <NavigationContainer>
+//       <Tab.Navigator tabBar={(props) => <TabBar {...props} />}>
+//         <Tab.Screen
+//           name="Home"
+//           component={HomeScreen}
+//           initialParams={{icon: 'home'}}
+//         />
+//         <Tab.Screen
+//           name="Groups"
+//           component={GroupStack}
+//           initialParams={{icon: 'addusergroup'}}
+//         />
+//         <Tab.Screen
+//           name="Map"
+//           component={MapScreen}
+//           initialParams={{icon: 'enviromento'}}
+//         />
+//       </Tab.Navigator>
+//     </NavigationContainer>
+//   );
+// };
+
+const TabBarNav = () => {
+  return (
+    <Tab.Navigator tabBar={(props) => <TabBar {...props} />}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        initialParams={{icon: 'home'}}
+      />
+      <Tab.Screen
+        name="Groups"
+        component={GroupScreen}
+        initialParams={{icon: 'addusergroup'}}
+      />
+      <Tab.Screen
+        name="Map"
+        component={MapScreen}
+        initialParams={{icon: 'enviromento'}}
+      />
+    </Tab.Navigator>
+  );
+};
 
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator tabBar={(props) => <TabBar {...props} />}>
-        <Tab.Screen name="Home" component={HomeScreen} initialParams={{icon:"home"}} />
-        <Tab.Screen name="Groups" component={GroupScreen} initialParams={{icon:"addusergroup"}} />
-        <Tab.Screen name="Map" component={MapScreen} initialParams={{icon:"enviromento"}} />
-      </Tab.Navigator>
+      <Stack.Navigator headerMode={false}>
+        <Stack.Screen name="Home" component={TabBarNav} />
+        <Stack.Screen name="Group" component={TabBarNav} />
+        <Stack.Screen name="Map" component={TabBarNav} />
+        <Stack.Screen name="GroupDetail" component={GroupDetailScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
--- src/screens/GroupDetailScreen.js
+++ src/screens/GroupDetailScreen.js
@@ -14,26 +14,36 @@ import AntDesign from 'react-native-vector-icons/AntDesign';
 
 const {width, height} = Dimensions.get('window');
 
-const GroupScreen = ({navigation}) => {
+const GroupDetailScreen = ({navigation}) => {
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={styles.container}>
         <View style={styles.headingContainer}>
-          <Text style={styles.heading}>Groups</Text>
+          <View
+            style={{
+              marginRight: 20,
+            }}>
+            <AntDesign
+              name="left"
+              size={25}
+              color={'#fe6666'}
+              onPress={() => navigation.goBack()}
+            />
+          </View>
+          <Text style={styles.heading}>Groups Details</Text>
         </View>
-        <View style={{marginTop:20,marginBottom: 190}}>
+        <View style={{marginTop: 20, marginBottom: 190}}>
           <ScrollView
             contentContainerStyle={{
               flexGrow: 1,
               width,
               paddingVertical: 20,
               paddingHorizontal: 10,
-            //   backgroundColor:"red"
+              //   backgroundColor:"red"
             }}>
             {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((k, i) => {
               return (
                 <TouchableOpacity
-                onPress={()=>navigation.navigate("GroupDetail")}
                   key={i}
                   activeOpacity={1}
                   style={styles.groupContainer}>
@@ -42,20 +52,14 @@ const GroupScreen = ({navigation}) => {
                       <AntDesign name="user" size={40} color={'#e6e6e6'} />
                     </View>
                     <View style={styles.groupDetail}>
-                      <Text>Group Name</Text>
-                      <Text style={{fontSize: 12}}>5 memebers</Text>
+                      <Text style={{fontSize: 15}}>Member Name</Text>
+                      {/* <Text style={{fontSize: 12}}>5 memebers</Text> */}
                     </View>
                   </View>
                 </TouchableOpacity>
               );
             })}
           </ScrollView>
-        </View>
-
-        <View style={styles.addGroupButtonContainer}>
-          <TouchableOpacity style={styles.addGroupButton}>
-            <AntDesign name="pluscircleo" size={40} color={'#fe6666'} />
-          </TouchableOpacity>
         </View>
       </View>
     </SafeAreaView>
@@ -76,9 +80,11 @@ const styles = StyleSheet.create({
     color: '#fe6666',
   },
   headingContainer: {
+    flexDirection: 'row',
     backgroundColor: '#fff',
     alignItems: 'center',
     padding: 10,
+    paddingTop: 20,
     width,
   },
   addGroupButtonContainer: {
@@ -114,8 +120,8 @@ const styles = StyleSheet.create({
   groupDetail: {
     paddingLeft: 10,
     paddingVertical: 5,
-    justifyContent: 'space-between',
+    justifyContent: 'center',
   },
 });
 
-export default GroupScreen;
+export default GroupDetailScreen;
